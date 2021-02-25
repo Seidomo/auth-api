badb2bdf68609761d37e3fea989f384a49fba4e6
seido <seid783@gmail.com>
2021-02-24 18:09:27 -0800

    change

---
+++ src/error-handlers/404.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// module.exports = (req,res,next) => {
+//   let error = { error: 'Resource Not Found' };
+//   res.statusCode = 404;
+//   res.statusMessage = 'Not Found';
+//   res.setHeader('Content-Type', 'application/json');
+//   res.write(JSON.stringify(error));
+//   res.end();
+// };
+
+module.exports = function (err, req, res, next) {
+
+  // Sometimes, errors come in as an object, others as a string
+  const error = err.message ? err.message : err;
+
+  const errorObject = {
+    status: 500,
+    message: error
+  }
+  res.status(500).json(errorObject);
+}
---
+++ src/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+///se
+const v1Routes = require('./auth/routes.js');
+const logger = require('./auth/middleware/logger.js');
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes.js');
+
+// Prepare the express app
+const app = express();
+ app.use(logger);
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(authRoutes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+ app.use('/api/v1', v1Routes);
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Server Up on ${port}`);
+    });
+  },
+};
